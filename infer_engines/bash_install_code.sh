eddd3c2e31ef67e4dde6ee02cb557d03f7a26cf6
Yao Yunxiang <yao.yunxiang@huawei.com>
2025-11-06 08:03:29 +0000

    !1130 add patch to fix bugs in first token reuse feature * fix one bug on boundary case * fix one typo in bash_install_code * Merge branch 'release_v0.6.0' of https://gitee.com/yyaoaj/omniinfer in… * add patch to fix bugs in first token reuse feature

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -37,3 +37,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/overload_control.patch
 git apply --whitespace=nowarn $PATCH_ROOT/enable_max_tokens_exclude_reasoning.patch
 git apply --whitespace=nowarn $PATCH_ROOT/guided_decoding_v3.2_adaption.patch
 git apply --whitespace=nowarn $PATCH_ROOT/adapt_chat_template_kwargs.patch
+git apply --whitespace=nowarn $PATCH_ROOT/fix_bugs_first_token.patch
