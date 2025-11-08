6c7f4ad100ca50dc5096f0235c26c72a5efca20a
xuzhou <13290430+xz6546@user.noreply.gitee.com>
2025-11-08 09:30:00 +0000

    !1174 修复reasoning卡住的问题 * 修复reasoning卡住的问题 * update infer_engines/bash_install_code.sh.

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -39,3 +39,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/guided_decoding_v3.2_adaption.patch
 git apply --whitespace=nowarn $PATCH_ROOT/adapt_chat_template_kwargs.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_bugs_first_token.patch
 git apply --whitespace=nowarn $PATCH_ROOT/guided_decoding_adapt_prefilled_token.patch
+git apply --whitespace=nowarn $PATCH_ROOT/patch_reasoning_thinking_bug.patch
