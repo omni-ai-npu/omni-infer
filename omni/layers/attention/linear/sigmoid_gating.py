6cc9cdadc546b938467331d5c0e12023f02133ca
KilluJ <jx6@illinois.edu>
2025-11-11 17:26:46 +0800

    update _fused_recurrent_gated_delta_rule_ref in sigmoid_gating.py

--- omni/layers/attention/linear/sigmoid_gating.py
+++ omni/layers/attention/linear/sigmoid_gating.py
@@ -206,8 +206,8 @@ def _fused_recurrent_gated_delta_rule_ref(
     hv_over_h = v.shape[-2] // q.shape[-2]
     if scale is None:
         scale = k.shape[-1]**-0.5
-    #Apply QK L2 norm if enabled
-    if use_qk_12norm_in_kernel:
+    #Apply QK L2 norm if enabled 
+    if use_qk_l2norm_in_kernel:
         q = q / (torch.linalg.norm(q, dim=-1, keepdim=True) + 1e-6)
         k = k / (torch.linalg.norm(k, dim=-1, keepdim=True) + 1e-6)
     q = q * scale
