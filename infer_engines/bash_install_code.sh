dae16ed7d534940ad79f40fffca63c7d212573af
Yao Yunxiang <yao.yunxiang@huawei.com>
2025-12-16 12:32:05 +0000

    !1655 add support of prefilled-token reusage for completion api * fix bugs * add patch to bash_install_code * add support of prefilled-token reusage for completion api

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -40,3 +40,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/adapt_chat_template_kwargs.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_bugs_first_token.patch
 git apply --whitespace=nowarn $PATCH_ROOT/guided_decoding_adapt_prefilled_token.patch
 git apply --whitespace=nowarn $PATCH_ROOT/patch_reasoning_thinking_bug.patch
+git apply --whitespace=nowarn $PATCH_ROOT/patch_support_prefilled_token_skip_tokenize_completion.patch
