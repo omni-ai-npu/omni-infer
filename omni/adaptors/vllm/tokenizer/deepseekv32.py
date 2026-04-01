af78ad4275720b6d42351ded241c45abb8be111c
hyf19981227 <huangyufei8@huawei.com>
2026-04-01 07:39:29 +0000

    !2169 Fix internal server error
    * Fix 500
    * Fix 500 error
    * Fix internal server error

--- omni/adaptors/vllm/tokenizer/deepseekv32.py
+++ omni/adaptors/vllm/tokenizer/deepseekv32.py
@@ -23,6 +23,8 @@ class DeepseekV32Tokenizer(HfTokenizer):
     @classmethod
     def validate_messages(cls, messages):
         try:
+            if len(messages) > 0 and messages[-1].get("role") == "assistant":
+                return False, f"Invalid consecutive assistant message at message index {len(messages) - 1}"
             for index in range(len(messages)):
                 msg = messages[index]
                 role = msg.get("role")
@@ -31,22 +33,6 @@ class DeepseekV32Tokenizer(HfTokenizer):
                 if role not in ["system", "developer", "user", "tool","assistant"]:
                     return False, f"Unkown role: {role}. Role just can be [system, developer, user, tool,assistant]"
 
-                if role == "developer":
-                    if not content:
-                        return False, f"Invalid message for {role}: {msg}"
-
-                elif role == "tool":
-                    pre_assistant_idx = index - 1
-                    assistant_msg = messages[pre_assistant_idx]
-                    while pre_assistant_idx >= 0 and assistant_msg.get("role") == "tool":
-                        pre_assistant_idx -= 1
-                        assistant_msg = messages[pre_assistant_idx]
-                    if not (index ==0 or pre_assistant_idx >= 0 and assistant_msg.get("role") == "assistant"):
-                        return False, f"Invalid message at {index}\n{assistant_msg}"
-                    tool_call_order = index - pre_assistant_idx
-                    assistant_tool_calls = assistant_msg.get("tool_calls")
-                    if not (assistant_tool_calls and len(assistant_tool_calls) >= tool_call_order):
-                        return False, "No tool calls but found tool output"
             return True, ""
         except Exception as e:
             return True, ""
@@ -99,7 +85,7 @@ class DeepseekV32Tokenizer(HfTokenizer):
 
         encode_config = dict(thinking_mode=thinking_mode, drop_thinking=drop_thinking)
         prompt_str = encode_messages(messages, **encode_config)  # type: ignore
-        if len(messages) > 0 and (messages[-1].get("role") == "assistant" or messages[-1].get("role") == "system"):
+        if len(messages) > 0 and messages[-1].get("role") == "system":
             if thinking_mode == "thinking":
                 prompt_str += "<｜Assistant｜> <think>"
             else:
