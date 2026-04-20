55c6d36775c8a17372d34e5118bb33a5db1d2725
hyf19981227 <huangyufei8@huawei.com>
2026-04-20 02:02:27 +0000

    !2206 Fix fragment of </think>
    * Fix fragment of </think>
    * Fix fragment of </think>
    * Fix fragment of </think>
    * fix frament of <think>

--- omni/adaptors/vllm/patches/thinking_tag_bug_fix_patch.py
+++ omni/adaptors/vllm/patches/thinking_tag_bug_fix_patch.py
@@ -1,42 +1,52 @@
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
 # -*- coding: utf-8 -*-
+from typing import Optional
 
-"""Sequence and its related classes."""
-from vllm.config import get_current_vllm_config
 from vllm.logger import init_logger
+from vllm.transformers_utils.tokenizer import AnyTokenizer
+from vllm.v1.engine import EngineCoreRequest
 from vllm.v1.engine.detokenizer import BaseIncrementalDetokenizer
+from vllm.v1.engine.detokenizer import IncrementalDetokenizer
 
 logger = init_logger(__name__)
 
-QWEN_3_THINK_START_TOKEN_ID = 151667  # <think>
-QWEN_3_THINK_END_TOKEN_ID = 151668  # </think>
-QWEN_3_AFTER_THINK_END_TOKEN_ID1 = 271  # \n\n
-QWEN_3_AFTER_THINK_END_TOKEN_ID2 = 9612  # \n
-
-DEEPSEEK_R1_THINK_START_TOKEN_ID = 128798 # <think>
-DEEPSEEK_R1_THINK_END_TOKEN_ID = 128799 # </think>
-DEEPSEEK_R1_AFTER_THINK_END_TOKEN_ID1 = 201 # \n
-DEEPSEEK_R1_AFTER_THINK_END_TOKEN_ID2 = 271 # \n\n
-
+COT_THINK_END_TOKEN = "</think>"
+DSML_TOKEN = "｜DSML｜"
 SLIDING_WINDOW_SIZE = -16
 
-BaseIncrementalDetokenizer._cot_end_symbol = False
-BaseIncrementalDetokenizer._check_len = None
+origin_from_new_request = IncrementalDetokenizer.from_new_request
+origin_init = BaseIncrementalDetokenizer.__init__
 
 
-def check_len(self):
-    vllm_config = get_current_vllm_config()
-    schedulerConfig = vllm_config.scheduler_config
-    if not self._check_len:
-        self._check_len = schedulerConfig.num_scheduler_steps * (
-                    vllm_config.speculative_config.num_speculative_tokens + 1) + 1
-    return self._check_len
+def __init__(self, request: EngineCoreRequest):
+    origin_init(self, request)
+    # Adapt: the symbol of COT end
+    self._cot_end_symbol = False
+
+
+@classmethod
+def from_new_request(
+    cls,
+    tokenizer: Optional[AnyTokenizer],
+    request: EngineCoreRequest
+) -> "IncrementalDetokenizer":
+    incremental_detokenizer: IncrementalDetokenizer = origin_from_new_request(tokenizer, request)
+
+    # Adapt:Init COT token
+    if tokenizer is not None:
+        incremental_detokenizer.set_cot_token_ids(tokenizer)
+    return incremental_detokenizer
+
+
+def set_cot_token_ids(self, tokenizer: Optional[AnyTokenizer]):
+    vocab = tokenizer.get_vocab()
+    self.cot_think_end_token_id = vocab.get(COT_THINK_END_TOKEN)
 
 
 def is_cot_end(self) -> bool:
-    # Return True: do not change max_tokens,
-    # Return False: change max_tokens
-
+    """
+    Determine whether the current output has completed the output of the chain of thought.
+    """
     if self._cot_end_symbol:
         return True
 
@@ -48,23 +58,9 @@ def is_cot_end(self) -> bool:
     # but we currently cannot obtain the vllm_config within the detokenizer process.
     check_token_ids = self.token_ids[SLIDING_WINDOW_SIZE:]
 
-    # Qwen 3 output ends with "</think>\n\n" or "</think>\n"
-    if QWEN_3_THINK_END_TOKEN_ID in check_token_ids:
-        check_index = check_token_ids.index(QWEN_3_THINK_END_TOKEN_ID) + 1
-        if check_index < len(check_token_ids) and check_token_ids[check_index] in [
-            QWEN_3_AFTER_THINK_END_TOKEN_ID1, QWEN_3_AFTER_THINK_END_TOKEN_ID2
-        ]:
-            cot_end = True
-
-    # Deepseek output ends with "</think>\n" or "</think>\n\n"
-    if DEEPSEEK_R1_THINK_END_TOKEN_ID in check_token_ids:
-        check_index = check_token_ids.index(DEEPSEEK_R1_THINK_END_TOKEN_ID) + 1
-        if check_index < len(check_token_ids) and check_token_ids[check_index] in [
-            DEEPSEEK_R1_AFTER_THINK_END_TOKEN_ID1, DEEPSEEK_R1_AFTER_THINK_END_TOKEN_ID2
-        ]:
-            cot_end = True
-
-
+    # COT ends with "</think>"
+    if self.cot_think_end_token_id in check_token_ids:
+        cot_end = True
     if cot_end:
         self._cot_end_symbol = True
 
@@ -76,8 +72,9 @@ def get_next_output_text(self, finished: bool, delta: bool) -> str:
     this method is returned"""
     # We return the full output text if the sequence is finished.
     thinking = not self.is_cot_end()
-    is_thinking_end_chunk = "</think>" in self.output_text[self._last_output_text_offset:]
-    buffer_length = 0 if finished or thinking or is_thinking_end_chunk else self.stop_buffer_length
+    is_thinking_end_chunk = COT_THINK_END_TOKEN in self.output_text[self._last_output_text_offset:]
+    is_DSML_chunk = DSML_TOKEN in self.output_text[self._last_output_text_offset:]
+    buffer_length = 0 if finished or thinking or is_thinking_end_chunk or is_DSML_chunk else self.stop_buffer_length
     if not delta:
         return self.output_text[:-buffer_length] if buffer_length else (
             self.output_text)
@@ -89,10 +86,10 @@ def get_next_output_text(self, finished: bool, delta: bool) -> str:
     return ""
 
 
-
 def patch_thinking_bug_fix():
     from vllm.v1.engine import detokenizer
-
+    detokenizer.IncrementalDetokenizer.from_new_request = from_new_request
+    detokenizer.BaseIncrementalDetokenizer.__init__ = __init__
     detokenizer.BaseIncrementalDetokenizer.get_next_output_text = get_next_output_text
     detokenizer.BaseIncrementalDetokenizer.is_cot_end = is_cot_end
-    detokenizer.BaseIncrementalDetokenizer.check_len = check_len
+    detokenizer.BaseIncrementalDetokenizer.set_cot_token_ids = set_cot_token_ids
