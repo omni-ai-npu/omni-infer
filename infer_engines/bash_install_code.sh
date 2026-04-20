780c248a86541b144ac4acc7f6b10d80418adf39
伏钡 <fubei5@huawei.com>
2026-04-20 06:48:37 +0000

    !2207 fix: enrich npu error log context
    * fix: enrich npu error log context

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -64,3 +64,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/fix_apc_mtp_conflict.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_multimtp.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_reasoning_content_to_content.patch
 git apply --whitespace=nowarn $PATCH_ROOT/validate_assistant_with_sufficient_tools.patch
+git apply --whitespace=nowarn $PATCH_ROOT/enrich_NPU_error_log_context.patch
